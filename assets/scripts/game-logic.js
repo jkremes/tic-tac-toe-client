0ce8d567292f1c8b8969c5dbd103278ad7ab09e2
jkremes <only1truth333@gmail.com>
2018-09-26 15:51:04 -0400

    I added game logic
    I added comments that would help me understand how to check if the game is over. I checked the API which suggest "over" and if that is true or not. I am matching the player_x and player_o against the different methods of winning and if the array length is equal to 9 and nobody has won then it is a tie game.

--- assets/scripts/game-logic.js
+++ assets/scripts/game-logic.js
@@ -6,9 +6,42 @@
 //  Can not choose already occupied spots
 //  Check Board for Winner
 
+//create gameboard
+const gameBoard = [
+  'top-left', 'top-middle', 'top-right',
+  'middle-left', 'middle-middle', 'middle-right',
+  'bottom-left', 'bottom-middle', 'bottom-right'
+]
+
+//create players
 const player_x = 'x'
 const player_o = 'o'
 
+//check if x/o won
+
+//x won if
+  //player_x is on 'top-left', 'top-middle', 'top-right',
+  //player_x is on 'middle-left', 'middle-middle', 'middle-right',
+  //player_x is on 'bottom-left', 'bottom-middle', 'bottom-right'
+  //player_x is on 'top-left', 'middle-middle', 'bottom-right',
+  //player_x is on 'top-left', 'middle-left', 'bottom-left',
+  //player_x is on 'top-middle', 'middle-middle', 'bottom-middle',
+  //player_x is on 'top-right', 'middle-right', 'bottom-right',
+  //player_x is on 'top-right', 'middle-middle', 'bottom-left',
+  //else if o wins...
+  //player_o is on 'top-left', 'top-middle', 'top-right',
+  //player_o is on 'middle-left', 'middle-middle', 'middle-right',
+  //player_o is on 'bottom-left', 'bottom-middle', 'bottom-right'
+  //player_o is on 'top-left', 'middle-middle', 'bottom-right',
+  //player_o is on 'top-left', 'middle-left', 'bottom-left',
+  //player_o is on 'top-middle', 'middle-middle', 'bottom-middle',
+  //player_o is on 'top-right', 'middle-right', 'bottom-right',
+  //player_o is on 'top-right', 'middle-middle', 'bottom-left',
+//draw if no player_o won or player_o won and gameboard [a,b,c,d,...] is full
+}
+//create turns array to log onto board array when clicked
+
+//create a turns array to check if the game is compl
 const turnLocation = function (clicks) {
   const turns = []
   for (let i = 0; i < 8; i++) {
@@ -21,3 +54,17 @@ const turnLocation = function (clicks) {
 }
 
 console.log(turnLocation())
+
+//create function to test the status of the game
+const checkGameStatus = () {
+  //check if player_x won
+    //player_x won if three in a row
+  //check if player_o won
+    //player_o won if three in a row
+  //otherwise draw...neither player_x or player_o had three in a row when all elements of the game board are filled out
+}
+
+//include styling
+//create x image
+//create o image
+//change gameBoard image to player image that clicked on it
