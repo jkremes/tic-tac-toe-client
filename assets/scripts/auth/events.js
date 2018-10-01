30fd672fff3bc4b9440d4105d3b284a98b8b5a05
jkremes <only1truth333@gmail.com>
2018-10-01 15:13:47 -0400

    Added gamelogic
    created checkWinner function and switchPlayer function. Need to incorporate logic into  ui.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -22,6 +22,7 @@
 const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
+const store = require('../store.js')
 
 const onSignUp = function (event) {
   event.preventDefault()
@@ -62,11 +63,100 @@ const onNewGame = function (event) {
 
 const onMove = function (event) {
   event.preventDefault()
+
   const gameData = getFormFields(event.target)
   api.move(gameData)
     .then(console.log)
     .catch(console.log)
 }
+
+// write a function that will switch the currentPlayer
+let currentPlayer = 'x'
+const switchPlayer = function () {
+  if (currentPlayer === 'x') {
+    currentPlayer = 'o'
+  } currentPlayer = 'x'
+}
+
+const checkWinner = function () {
+  let winner = ""
+  if (store.game.cells[0] === store.game.cells[1] && store.game.cells[0] === store.game.cells[2] && store.game.cells[0] !== '') {
+    winner = store.game.cells[0]
+  } else if (store.game.cells[3] === store.game.cells[4] && store.game.cells[3] === store.game.cells[5] && store.game.cells[3] !== '') {
+    winner = store.game.cell[3]
+  } else if (store.game.cells[6] === store.game.cells[7] && store.game.cells[6] === store.game.cells[8] && store.game.cells[6] !== '') {
+    winner = store.game.cell[6]
+  } else if (store.game.cells[0] === store.game.cells[3] && store.game.cells[0] === store.game.cells[6] && store.game.cells[0] !== '') {
+    winner = store.game.cell[0]
+  } else if (store.game.cells[1] === store.game.cells[4] && store.game.cells[1] === store.game.cells[7] && store.game.cells[1] !== '') {
+    winner = store.game.cell[1]
+  } else if (store.game.cells[2] === store.game.cells[5] && store.game.cells[2] === store.game.cells[8] && store.game.cells[2] !== '') {
+    winner = store.game.cell[2]
+  } else if (store.game.cells[0] === store.game.cells[4] && store.game.cells[0] === store.game.cells[8] && store.game.cells[0] !== '') {
+    winner = store.game.cell[0]
+  } else if (store.game.cells[2] === store.game.cells[4] && store.game.cells[2] === store.game.cells[6] && store.game.cells[2] !== '') {
+    winner = store.game.cell[2]
+  }
+  store.game.over = true
+  switchPlayer()
+}
+
+// if (currentPlayer === store.game.cells[0 && 1 && 2] ||
+//   currentPlayer === store.game.cells[3 && 4 && 5] ||
+//   currentPlayer === store.game.cells[6 && 7 && 8] ||
+//   currentPlayer === store.game.cells[0 && 3 && 6] ||
+//   currentPlayer === store.game.cells[1 && 4 && 7] ||
+//   currentPlayer === store.game.cells[2 && 5 && 8] ||
+//   currentPlayer === store.game.cells[0 && 4 && 8] ||
+//   currentPlayer === store.game.cells[2 && 4 && 6] ||)
+// const switchToken = function () {
+//  data.game.value =
+// {
+// "game": {
+//   "cell": {
+//     "index": 0,
+//     "value": "x"
+//   },
+//   "over": false
+//   }
+// }
+//   }
+// }
+
+// check the winner
+// evaluate which value for current player is in the array
+// check if x won, if not check if o won, if true change gameData to 'True'
+// if false then switch current player
+
+// let currentPlayer = x
+
+// winnning conditions - x
+// ["x", "x", "x", "", "", "", "", "", ""]
+// ["", "", "", "x", "x", "x", "", "", ""]
+// ["", "", "", "", "", "", "x", "x", "x"]
+// ["x", "", "", "x", "", "", "x", "", ""]
+// ["", "x", "", "", "x", "", "", "x", ""]
+// ["", "", "x", "", "", "x", "", "", "x"]
+// ["x", "", "", "", "x", "", "", "", "x"]
+// ["", "", "x", "", "x", "", "x", "", ""]
+// winning conditions - o
+// ["o", "o", "o", "", "", "", "", "", ""]
+// ["", "", "", "o", "o", "o", "", "", ""]
+// ["", "", "", "", "", "", "o", "o", "o"]
+// ["o", "", "", "o", "", "", "o", "", ""]
+// ["", "o", "", "", "o", "", "", "o", ""]
+// ["", "", "o", "", "", "o", "", "", "o"]
+// ["o", "", "", "", "o", "", "", "", "o"]
+// ["", "", "o", "", "o", "", "o", "", ""]
+// if one of these are true then ui
+// currentPlayer has won the game!
+
+// if currentPlayer is 'x'
+// then currentPlayer = 'o'
+// if currentPlayer is 'o'
+// then currentPlayer = 'x'
+
+
 
 module.exports = {
   onSignUp,
